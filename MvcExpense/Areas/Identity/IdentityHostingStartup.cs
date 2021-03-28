2341a19a9fd28b469efc748f1fbd839cfe67c7ca
Berei Sanyi <berei_sanyi@yahoo.com>
2021-03-29 02:04:50 +0300

    Expense Manager App

---
+++ MvcExpense/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcExpense.Areas.Identity.Data;
+using MvcExpense.Data;
+
+[assembly: HostingStartup(typeof(MvcExpense.Areas.Identity.IdentityHostingStartup))]
+namespace MvcExpense.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MvcExpenseDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MvcExpenseDbContextConnection")));
+
+                services.AddDefaultIdentity<MvcExpenseUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<MvcExpenseDbContext>();
+            });
+        }
+    }
+}
---
+++ MvcExpense/Data/MvcExpenseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcExpense.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcExpense.Data
+{
+    public class MvcExpenseContext : DbContext
+    {
+        public MvcExpenseContext(DbContextOptions<MvcExpenseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expense { get; set; }
+    }
+}
